1674747fe8441aa7eef9835d2a094bce414ae14b
JZITNIK-github <kintiz@seznam.cz>
2023-08-12 09:45:47 +0200

    created version 2

---
+++ README.md
@@ -0,0 +1,70 @@
+# Automatic bio changer
+
+Automatic bio changer is program that will automatically change your bio on supported platforms.
+
+## Supported platforms
+
+1. Github
+2. Discord
+
+## Disclaimer
+
+If you use it with discord **it is against the DISCORD'S TERMS OF SERVICE**.
+You can get banned from Discord.
+**Use this program at your own risk.**
+
+## Features
+
+1. Web interface
+2. Custom bio template
+3. Custom configuration
+
+## Installation
+
+You can install this program to you own server.
+
+### 1. Clone this repository
+
+```
+git clone https://github.com/JZITNIK-github/automatic-bio-changer
+```
+
+### 2. Go to folder
+
+```
+cd automatic-bio-changer
+```
+
+### 3. Install required libraries
+
+```
+pip install -r requirements.txt
+```
+
+### 4. Run the server
+
+```
+python3 main.py
+```
+
+## How to get tokens
+
+### Discord
+
+[Here is the link to the tutorial for discord token.](https://www.androidauthority.com/get-discord-token-3149920/)
+
+### Github
+
+1. Go to [this link](https://github.com/settings/tokens)
+2. Click "Generate new token"
+
+![Image](images/token/github/1.png)
+
+3. Click "Generate new token (classic)"
+4. Set "Note" to "change-bio" and "expiration" to "No expiration"
+5. Scroll down a bit and select "user"
+
+![Image](images/token/github/2.png)
+
+6. Click "Generate token"
+7. Copy generated token and paste it into settings
---
+++ scripts/changer.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+class Changer:
+    def discord(text, token):
+        try:
+            r = requests.patch(
+                url="https://discord.com/api/v9/users/@me",
+                headers={"authorization": token},
+                json={"bio": text},
+            )
+        except requests.exceptions.ConnectTimeout:
+            return "noconnection"
+        else:
+            response = json.loads(r.content)
+            if response.get("message", None) != None:
+                if "401" in response["message"]:
+                    return "notvalidtoken"
+                else:
+                    return "unknownerror"
+            else:
+                return "success"
+
+    def github(text, token):
+        try:
+            response = requests.patch(
+                "https://api.github.com/user",
+                headers={"Authorization": "Bearer " + token},
+                data=json.dumps({"bio": text}),
+            )
+        except requests.exceptions.ConnectionError:
+            return "noconnection"
+        else:
+            if response.status_code == 200:
+                return "success"
+            elif response.status_code == 304:
+                return "cannotchange"
+            elif response.status_code == 401 or response.status_code == 403:
+                return "notvalidtoken"
+            elif response.status_code == 422:
+                return "toolong"
+            else:
+                return "unknownerror"
